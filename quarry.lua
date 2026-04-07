e3768ef395a37c0726bc0a24dc7df8a7e4ab2ff7
Drake Scott <ArbiterBibendi@yahoo.com>
2026-04-07 15:33:22 -0500

    frame work

--- quarry.lua
+++ quarry.lua
@@ -1,15 +1,78 @@
+local DIR_FORWARD, DIR_RIGHT, DIR_BACKWARD, DIR_LEFT = 0, 1, 2, 3
 local x, y, z = 0, 0, 0
 local qx, qy, qz = 4, 4, 4
+local direction = 0
 
-while (turtle.getFuelLevel() - 1) + y >= 0 do
+function face(dir)
+	while direction ~= dir do
+		if direction ~= dir then
+			turtle.turnRight()
+			direction = direction + 1
+			if direction > 3 then
+				direction = 0
+			end
+		end
+	end
+end
+function turtleReturn()
+	while y < 0 do -- turtle only goes down
+		turtle.up()
+		y = y + 1
+	end
+
+	local direction_x = x / math.abs(x)
+	local direction_z = z / math.abs(z)
+
+	while x ~= 0 do
+		if direction_x == -1 then
+			face(DIR_RIGHT)
+		elseif direction_x == 1 then
+			face(DIR_LEFT)
+		end
+		forward()
+	end
+	while z ~= 0 do
+		if direction_z == -1 then
+			face(DIR_FORWARD)
+		elseif direction_z == 1 then
+			face(DIR_BACKWARD)
+		end
+		forward()
+	end
+end
+
+function checkFuel()
+	if (turtle.getFuelLevel() - 10) - (math.abs(x) + math.abs(y) + math.abs(z)) >= 0 then
+		return true
+	end
+	return false
+end
+
+function forward()
+	if turtle.forward() == false then
+		turtle.dig()
+	else
+		if direction == DIR_FORWARD then
+			z = z + 1
+		elseif direction == DIR_BACKWARD then
+			z = z - 1
+		elseif direction == DIR_LEFT then
+			x = x - 1
+		elseif direction == DIR_RIGHT then
+			x = x + 1
+		end
+	end
+end
+function down()
 	if turtle.down() == false then
 		turtle.digDown()
-		term.write(turtle.getFuelLevel())
+	else
+		y = y - 1
 	end
-	y = y - 1
 end
 
-while y < 0 do
-	turtle.up()
-	y = y + 1
+while checkFuel() do
+	forward()
 end
+
+turtleReturn()
