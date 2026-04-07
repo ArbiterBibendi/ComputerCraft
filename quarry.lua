1529929179ebb3a8bce696fa5f8caf90e130b0be
Drake Scott <ArbiterBibendi@yahoo.com>
2026-04-07 13:44:12 -0500

    watch

--- quarry.lua
+++ quarry.lua
@@ -1,7 +1,7 @@
 local x, y, z = 0, 0, 0
 local qx, qy, qz = 4, 4, 4
 
-while (turtle.getFuelLevel() - 1) + y <= 0 do
+while (turtle.getFuelLevel() - 1) + y >= 0 do
 	if turtle.down() == false then
 		turtle.digDown()
 		term.write(turtle.getFuelLevel())
