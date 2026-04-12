60880ea74b5a42a6e6dd0ce23c20801388a381e7
Drake Scott <ArbiterBibendi@yahoo.com>
2026-04-12 12:13:36 -0500

    idk man

--- quarry.lua
+++ quarry.lua
@@ -1,17 +1,40 @@
 local DIR_FORWARD, DIR_RIGHT, DIR_BACKWARD, DIR_LEFT = 0, 1, 2, 3
 local x, y, z = 0, 0, 0
-local qx, qy, qz = 4, 4, 4
+local qx, qy, qz = 3, 3, 3
 local direction = 0
 
 function face(dir)
 	while direction ~= dir do
-		if direction ~= dir then
-			turtle.turnRight()
-			direction = direction + 1
-			if direction > 3 then
-				direction = 0
-			end
+		turtle.turnRight()
+		direction = direction + 1
+		if direction > 3 then
+			direction = 0
 		end
+	end
+end
+
+function forward()
+	if turtle.forward() == false then
+		turtle.dig()
+		forward()
+	else
+		if direction == DIR_FORWARD then
+			z = z + 1
+		elseif direction == DIR_BACKWARD then
+			z = z - 1
+		elseif direction == DIR_LEFT then
+			x = x - 1
+		elseif direction == DIR_RIGHT then
+			x = x + 1
+		end
+	end
+end
+function down()
+	if turtle.down() == false then
+		turtle.digDown()
+		down()
+	else
+		y = y - 1
 	end
 end
 function turtleReturn()
@@ -48,31 +71,58 @@ function checkFuel()
 	return false
 end
 
-function forward()
-	if turtle.forward() == false then
-		turtle.dig()
+local direction_to_mine_z, direction_to_mine_x = DIR_FORWARD, DIR_RIGHT
+
+function mineX()
+	face(direction_to_mine_x)
+	forward()
+end
+function mineZ()
+	face(direction_to_mine_z)
+	forward()
+end
+function swapX()
+	if direction_to_mine_x == DIR_RIGHT then
+		direction_to_mine_x = DIR_LEFT
 	else
-		if direction == DIR_FORWARD then
-			z = z + 1
-		elseif direction == DIR_BACKWARD then
-			z = z - 1
-		elseif direction == DIR_LEFT then
-			x = x - 1
-		elseif direction == DIR_RIGHT then
-			x = x + 1
-		end
+		direction_to_mine_x = DIR_RIGHT
 	end
 end
-function down()
-	if turtle.down() == false then
-		turtle.digDown()
+function swapZ()
+	if direction_to_mine_z == DIR_FORWARD then
+		direction_to_mine_z = DIR_BACKWARD
 	else
-		y = y - 1
+		direction_to_mine_z = DIR_FORWARD
 	end
 end
 
-while checkFuel() do
-	forward()
+down()
+for i = 1, qy do
+	for j = 1, qz do
+		for k = 1, qx - 1 do
+			mineX()
+			if checkFuel() == false then
+				turtleReturn()
+				break
+			end
+		end
+		swapX()
+		if j < qz then
+			mineZ()
+			if checkFuel() == false then
+				turtleReturn()
+				break
+			end
+		end
+	end
+	swapZ()
+	if i < qy then
+		down()
+		if checkFuel() == false then
+			turtleReturn()
+			break
+		end
+	end
 end
 
 turtleReturn()
